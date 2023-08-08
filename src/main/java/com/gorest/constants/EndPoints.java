a264bc335d0035b58cbae7a73afb330f80b0fdc9
anupatel14uk <anupatel14uk@gmail.com>
2023-08-08 21:41:01 +0100

    serinityhw

---
+++ src/main/java/com/gorest/constants/EndPoints.java
@@ -0,0 +1,13 @@
+package com.gorest.constants;
+
+
+public class EndPoints {
+
+    /**
+     * This is Endpoints of Products
+     */
+    public static final String CREATE_USER = "/users";
+    public static final String UPDATE_USER_BY_id = "/{userID}";
+    public static final String DELETE_USER_BY_id = "/{userID}";
+    public static final String GET_USER_BY_id = "/{userID}";
+}
---
+++ src/main/java/com/gorest/constants/Path.java
@@ -0,0 +1,9 @@
+package com.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    public static final String USERS = "/users";
+
+}
